875c1e8fc2a2a5c7868e4cddc105fa82cc741c55
Nick Scaglione <nicholas.a.scaglione@gmail.com>
2016-09-12 21:52:57 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
 def my_select(collection)
- # your code here!
+  counter = 0
+  selected = Array.new
+  while counter < collection.size do
+    if yield(collection[counter])
+      selected.push(collection[counter])
+    end
+    counter += 1
+  end
+  selected
 end
